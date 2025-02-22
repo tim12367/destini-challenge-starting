96ed349ceeeb986acc0bf41aa4f1fb3a81def613
tim12367 <tim12367@ymail.com>
2025-02-22 21:44:36 +0800

    使用 decoration 設定Container 背景圖片

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,14 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //TODO: Step 1 - Add background.png to this Container as a background image.
+        // Step 1 - Add background.png to this Container as a background image.
+        // 使用decoration設定背景圖片
+        decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage("images/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
